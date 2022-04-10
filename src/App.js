7018fc83a2c36342c1f1f826ff65944ecba369d9
chencheng1231 <yuchenghsu1231@gmail.com>
2022-04-10 15:29:29 +0800

    add ui component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { Modal, CloseButton, Toast, Card } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Toast style={{ width: '50vw', background: 'white', margin: '60px auto 0px auto' }}>
+        <Modal.Header>
+          <Modal.Title>Todo List</Modal.Title>
+        </Modal.Header>
+        <section className='add-todo-bar'>
+          <input id="new-task" type="text" placeholder="Add something to do here σ`∀´)σ" />
+          <button className='add-btn'>Add</button>
+        </section>
+        
+        <Modal.Body style={{ textAlign: 'start'}}>
+          <Card.Body style={{ display: 'flex', justifyContent: 'space-between'}}>
+            This is some text within a card body.
+            <CloseButton />
+          </Card.Body>
+          <Card.Body style={{ display: 'flex', justifyContent: 'space-between'}}>
+            This is some text within a card body.
+            <CloseButton />
+          </Card.Body>
+        </Modal.Body>
+      </Toast>
     </div>
   );
 }
